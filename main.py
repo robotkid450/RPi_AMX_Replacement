7b5d9e20232015adf45a4898fbd21cd37c2baf55
Josh Smith <robotkid450@gmail.com>
2017-05-27 10:56:25 -0500

    test

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 #! /usr/bin/env python3
+
+import RPi.GPIO as GPIO
+
+driverPin = 11
+multiplexPins = [12, 13, 15, 16, 18, 22, 29]
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(17, GPIO.OUT, False)
+    GPIO.setup(12, GPIO.OUT, False)
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
